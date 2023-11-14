bcb3ca84b1cb625aea8c5cf47c636c02545a52b3
Laviel <fafansin@gmail.com>
2023-11-14 09:46:58 +0300

    updating is down for markdown

--- src/markdown/Markdown.jsx
+++ src/markdown/Markdown.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import { useState } from 'react';
+import { marked } from 'marked';
 
-class Markdown extends React.Component{
-    render(){
-        return(
-            <h1>Markdown</h1>
-        )
-    }
+export default function Markdown (){
+  const [ markup, setMarkup] = useState('Singapore')
+  
+  return(
+    <div className="markdown">
+      <textarea name="editor" 
+        id="editor" 
+        cols="30" 
+        rows="10" 
+        value={markup}
+        onChange={(event) => setMarkup(event.target.value)}
+        ></textarea>
+      <div id="preview" dangerouslySetInnerHTML={{__html:marked.parse(markup)}}></div>
+    </div>
+  )
 }
-
-export default Markdown
