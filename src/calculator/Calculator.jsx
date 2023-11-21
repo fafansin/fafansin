73354c814e99e4d7a291cf60eed89c4f1f7a9094
Laviel <fafansin@gmail.com>
2023-11-21 13:51:39 +0300

    Calculator Code cleanup part 1

--- src/calculator/Calculator.jsx
+++ src/calculator/Calculator.jsx
@@ -7,17 +7,14 @@ const Calculator = () => {
   
   const regex = /[0-9]|\./;
 
-  const numbers = [1,2,3,4,5,6,7,8,9,0,'.'];
-  const operations = ['+', '-', "*", "/"];
-    
   function onPress(event){
     event.preventDefault();
     const value = event.target.value;
     
-    const isNumber = numbers.find(item => item == value) != undefined;
-    const isOperation = operations.find(item => item == value) != undefined;
+    const isNumber = regex.test(value);
+    const isOperation = !regex.test(value);
     
-    const isFirstChar = display == 0;
+    const isFirstChar = parseInt(display) === 0;
     const lastChar = formula[formula.length -1 ];
     //
     const idx = formula.indexOf("=");
@@ -55,14 +52,16 @@ const Calculator = () => {
           
           let form = solved ? solved : formula;
           let disp = solved ? solved : display;
+          
           const lastChar = disp[disp.length -1];
           const secondToLast = form[form.length -2];
-          if(numbers.find(item => item == lastChar) != undefined) { //kung number ang last char
+
+          if(regex.test(lastChar)) { //kung number ang last char
             setDisplay(value);
             setFormula(`${form}${value}`);
           }else{
             setDisplay(value);
-            if(secondToLast != undefined && operations.find(item => item == secondToLast) != undefined){
+            if(secondToLast !== undefined && !regex.test(secondToLast)){
               setFormula(`${form.substring(0,form.length - 2)}${value}`);
             }else{
               setFormula(`${form.substring(0,form.length - 1)}${value}`);
@@ -76,69 +75,13 @@ const Calculator = () => {
           setFormula(value);
         }else{
           const lastChar = formula[formula.length -1];
-          if(numbers.find(item => item == lastChar) != undefined || lastChar == '-') { 
+          if(regex.test(lastChar) || lastChar === '-') { 
             setDisplay(`${display}${value}`);
           }else{
             setDisplay(value);
           }
           setFormula(`${formula}${value}`);
         }
-      break;
-      case value === '=':
-        const regex = /[0-9]|\./;
-        let temp = '';
-        let all = [];
-        
-        for(let i =0; i < formula.length; i++){
-          let curr = formula[i];
-          let prev = formula[i-1]
-          
-          if(regex.test(curr)){
-            temp += curr;
-          }else{
-            if(regex.test(prev)){ // Operator ka tas ang previous ay number
-              all.push(parseFloat(temp));  
-              all.push(curr);
-              temp = '';
-            }else{ //Operator ka tas previous mo operator din. malamang negative ka
-              temp = curr;
-            }
-          }
-        }
-        all.push(parseFloat(temp));
-        
-        if(isNaN(all[all.length-1])){
-          all.pop()
-        }
-        if(isNaN(all[all.length-1])){
-          all.pop()
-        }
-        
-        let total = all.reduce((prev, curr) => {
-          if(typeof curr == 'number'){
-            switch(prev.operation){
-              case "+":
-                return {total:prev.total + curr, operation:prev.operation}
-                break;
-              case '-':
-                return {total:prev.total - curr, operation:prev.operation}
-                break;
-              case '/':
-                console.log("DIVIDE")
-                return {total:prev.total / curr, operation:prev.operation}
-                break;
-              case '*':
-                console.log("MULTIPLY")
-                return {total:prev.total * curr, operation:prev.operation}
-                break;
-            }
-          }else{
-            return {total:prev.total, operation:curr}
-          }
-        }, {total:0, operation:'+'})
-        
-        setDisplay(total.total);
-        setFormula(`${formula}=${total.total}`);
       break;
       default:
         console.log('LONER SA DEFAULT');
@@ -148,8 +91,70 @@ const Calculator = () => {
   }
 
 
-  function onDecimal(event){
-    console.log(event.target.value);
+  function onEquals(event){
+    event.preventDefault();
+    if(formula.includes('=')){
+      return
+    }
+    const regex = /[0-9]|\./;
+    let temp = '';
+    let all = [];
+    
+    for(let i =0; i < formula.length; i++){
+      let curr = formula[i];
+      let prev = formula[i-1]
+      
+      if(regex.test(curr)){
+        temp += curr;
+      }else{
+        if(regex.test(prev)){ // Operator ka tas ang previous ay number
+          all.push(parseFloat(temp));  
+          all.push(curr);
+          temp = '';
+        }else{ //Operator ka tas previous mo operator din. malamang negative ka
+          temp = curr;
+        }
+      }
+    }
+    all.push(parseFloat(temp));
+    
+    if(isNaN(all[all.length-1])){
+      all.pop()
+    }
+    if(isNaN(all[all.length-1])){
+      all.pop()
+    }
+    
+    let total = all.reduce((prev, curr) => {
+      let ref;
+      if(typeof curr === 'number'){
+        switch(prev.operation){
+          case "+":
+            ref = {total:prev.total + curr, operation:prev.operation}
+            break;
+          case '-':
+            ref = {total:prev.total - curr, operation:prev.operation}
+            break;
+          case '/':
+            ref = {total:prev.total / curr, operation:prev.operation}
+            break;
+          case '*':
+            ref = {total:prev.total * curr, operation:prev.operation}
+            break;
+          default:
+            // do nothing
+          break;
+        }
+      }else{
+        ref = {total:prev.total, operation:curr}
+      }
+      return ref;
+    }, {total:0, operation:'+'})
+    
+    setDisplay(total.total);
+    setFormula(`${formula}=${total.total}`);
+  
+
   }
 
   function onClear(event){
@@ -183,7 +188,7 @@ const Calculator = () => {
         <button className="pad" id="two" onClick={onPress} value="2">2</button>
         <button className="pad" id="three" onClick={onPress} value="3">3</button>
 
-        <button className="pad tall" id="equals" onClick={onPress} value="=">=</button>
+        <button className="pad tall" id="equals" onClick={onEquals} value="=">=</button>
         
         <button className="pad wide" id="zero" onClick={onPress} value="0">0</button>
         <button className="pad" id="decimal" onClick={onPress} value=".">.</button>
