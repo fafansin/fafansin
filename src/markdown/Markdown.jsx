c73101f87c93e67ae7034b37b10f36f494612d59
Laviel <fafansin@gmail.com>
2023-11-27 13:05:32 +0300

    used accordion for markdown page

--- src/markdown/Markdown.jsx
+++ src/markdown/Markdown.jsx
@@ -1,21 +1,34 @@
 import { useState } from 'react';
 import { marked } from 'marked';
 import initial from './initial.js';
+import { Accordion } from 'react-bootstrap';
+import  "./Markdown.scss";
 
 marked.use({breaks:true})
+
 export default function Markdown (){
   const [ markup, setMarkup] = useState(initial)
   
   return(
     <div className="markdown">
-      <textarea name="editor" 
-        id="editor" 
-        cols="30" 
-        rows="10" 
-        value={markup}
-        onChange={(event) => setMarkup(event.target.value)}
-        ></textarea>
-      <div id="preview" dangerouslySetInnerHTML={{__html:marked.parse(markup)}}></div>
+      <Accordion defaultActiveKey={['0', '1']} alwaysOpen data-bs-theme="dark" className="bg-secondary border">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Markdown Editor</Accordion.Header>
+          <Accordion.Body>
+            <textarea name="editor" 
+              id="editor" 
+              value={markup}
+              onChange={(event) => setMarkup(event.target.value)}
+              ></textarea>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Markdown Previewer</Accordion.Header>
+          <Accordion.Body>
+          <div id="preview" dangerouslySetInnerHTML={{__html:marked.parse(markup)}}></div>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     </div>
   )
 }
--- src/quotes/Quotes.jsx
+++ src/quotes/Quotes.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import list from './list.js';
-import './Quotes.css';
+import './Quotes.scss';
 import { Button } from 'react-bootstrap';
 
 export default function Quote(){
@@ -23,15 +23,17 @@ export default function Quote(){
   function getRandomColor(){
     const rands = [];
     for(let i=0; i < 3; i++){
-      rands.push(Math.floor(Math.random() * 255));
+      rands.push(Math.floor(Math.random()*150));
     }
     return `rgb(${rands.join(',')})`;
   }
   return(
-    <div className="quote-wrap px-1 px-md-5 " style={{backgroundColor:color}}>
+    <div className="quotes px-1 px-md-5 " style={{backgroundColor:color}}>
       <div id="quote-box" className="jumbotron border shadow rounded p-4" style={{color:color, backgroundColor:"#ffffff"}}>
         <blockquote>
-          <h1 className="display-6" id="text">{`" ${list[indexer].quote} "`}</h1>
+          <h1 className="display-6" id="text">
+            {`" ${list[indexer].quote} "`}
+          </h1>
           <footer id="author" className="lead">{`~ ${list[indexer].author}`}</footer>
         </blockquote>
         <div className="d-flex justify-content-end gap-2">
@@ -39,13 +41,15 @@ export default function Quote(){
             id="new-quote" 
             size="sm" 
             style={{backgroundColor:color, border:"none"}}
-            onClick={getRandom}>New Quote</Button>
+            onClick={getRandom}>New</Button>
           <a href="https://twitter.com/intent/tweet" 
             target="_blank" 
             rel="noreferrer" 
             id="tweet-quote" 
             style={{backgroundColor:color, border:"none"}}
-            className="btn btn-primary btn-small">Tweet Me</a>
+            className="btn btn-primary btn-small">
+              Twitter
+          </a>
         </div>
       </div>
     </div>
