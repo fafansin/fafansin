11566b8309e5a58df0158074d6c548c566de4c08
Laviel <fafansin@gmail.com>
2023-11-28 12:54:07 +0300

    completed layout and style for calculator

--- src/calculator/Calculator.jsx
+++ src/calculator/Calculator.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './Calculator.scss';
+import { Button } from 'react-bootstrap';
 
 const Calculator = () => {
   const [ formula, setFormula ] = useState('');
@@ -165,35 +166,29 @@ const Calculator = () => {
 
   return(
     <div className="calculator-wrap">
-      <div className="Calculator">
+      <div className="Calculator border shadow">
         <div className="display-wrap">
           <div id="formula">{ formula }</div>
           <div id="display">{ display }</div>
         </div>
         <div className="pads-wrap">
-          <button className="pad wide" id="clear" onClick={onClear} value="AC">AC</button>
-          <button className="pad operation" id="divide" onClick={onPress} value="/">/</button>
-          <button className="pad operation" id="multiply" onClick={onPress} value="*">x</button>
-
-          <button className="pad" id="seven" onClick={onPress} value="7">7</button>
-          <button className="pad" id="eight" onClick={onPress} value="8">8</button>
-          <button className="pad" id="nine" onClick={onPress} value="9">9</button>
-          <button className="pad operation" id="subtract" onClick={onPress} value="-">-</button>
-
-          <button className="pad" id="four" onClick={onPress} value="4">4</button>
-          <button className="pad" id="five" onClick={onPress} value="5">5</button>
-          <button className="pad" id="six" onClick={onPress} value="6">6</button>
-          <button className="pad operation" id="add" onClick={onPress} value="+">+</button>
-
-          <button className="pad" id="one" onClick={onPress} value="1">1</button>
-          <button className="pad" id="two" onClick={onPress} value="2">2</button>
-          <button className="pad" id="three" onClick={onPress} value="3">3</button>
-
-          <button className="pad tall" id="equals" onClick={onEquals} value="=">=</button>
-          
-          <button className="pad wide" id="zero" onClick={onPress} value="0">0</button>
-          <button className="pad" id="decimal" onClick={onPress} value=".">.</button>
-          
+          <Button className="pad wide" variant="danger" id="clear" onClick={onClear} value="AC">AC</Button>
+          <Button className="pad operation" variant="dark" id="divide" onClick={onPress} value="/">/</Button>
+          <Button className="pad operation" variant="dark" id="multiply" onClick={onPress} value="*">x</Button>
+          <Button className="pad" variant="secondary" id="seven" onClick={onPress} value="7">7</Button>
+          <Button className="pad" variant="secondary" id="eight" onClick={onPress} value="8">8</Button>
+          <Button className="pad" variant="secondary" id="nine" onClick={onPress} value="9">9</Button>
+          <Button className="pad operation" variant="dark" id="subtract" onClick={onPress} value="-">-</Button>
+          <Button className="pad" variant="secondary" id="four" onClick={onPress} value="4">4</Button>
+          <Button className="pad" variant="secondary" id="five" onClick={onPress} value="5">5</Button>
+          <Button className="pad" variant="secondary" id="six" onClick={onPress} value="6">6</Button>
+          <Button className="pad operation" variant="dark" id="add" onClick={onPress} value="+">+</Button>
+          <Button className="pad" variant="secondary" id="one" onClick={onPress} value="1">1</Button>
+          <Button className="pad" variant="secondary" id="two" onClick={onPress} value="2">2</Button>
+          <Button className="pad" variant="secondary" id="three" onClick={onPress} value="3">3</Button>
+          <Button className="pad tall" id="equals" variant="primary" onClick={onEquals} value="=">=</Button>
+          <Button className="pad wide" variant="secondary" id="zero" onClick={onPress} value="0">0</Button>
+          <Button className="pad" variant="secondary" id="decimal" onClick={onPress} value=".">.</Button>
         </div>
       </div>
     </div>
