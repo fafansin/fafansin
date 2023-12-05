e48ce56a323c75b0612fa81ec18edb6896a412d5
Laviel <fafansin@gmail.com>
2023-12-05 13:09:11 +0300

    guessing game functionality complete

--- src/guess/Guess.jsx
+++ src/guess/Guess.jsx
@@ -1,24 +1,30 @@
 import { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { v4 as uuidv4 } from 'uuid';
+import "./Guess.scss";
 
 function Guess() {
   const min = 1;
-  const max = 100;
   //
   const [ answers, setAnswers ] = useState([]);
   const [ display, setDisplay ] = useState('');
   const [ answer, setAnswer ] = useState(null);
+  const [ max, setMax ] = useState(100);
   
-  function onSubmit(event){
+  function onStart(event){
     event.preventDefault();
-    console.log(event.target.user.value);
-    const entered = event.target.user.value;
-    if(answer === null){
-      setDisplay('Press Start 1st');
-      return;
-    }
+    setAnswer(getRandomNumber());
+  }
 
+  function onChange(event){
+    setMax(event.target.value);
+  }
+
+  function onGuess(event){
+    event.preventDefault();
+    
+    const entered = event.target.guess.value;
+    console.log("This is the ", answer)
     let stat;  
     if(answer < entered ){
       stat = 'Try Lower';
@@ -31,22 +37,22 @@ function Guess() {
     setAnswers([...answers, `${entered} - ${stat}`]);
   }
 
-  function onStart(event){
-    event.preventDefault();
-    setAnswer(getRandomNumber());
-  }
-
   function getRandomNumber(){
     return Math.floor(Math.random() * (max-min + 1)) + min;
   }
 
   return (
-    <div className="guess">
-      <Button variant="primary" onClick={onStart}>Let's Go!</Button>
-      <Form onSubmit={onSubmit} className="my-5">
+    <div className="guess shadow container">
+      <Form onSubmit={onStart}>
+        <Form.Label>Give me a High Number</Form.Label>
+        <Form.Control type="number" name="maximum" value={max} step={1} min={min} onChange={onChange}/>
+        <Button type="submit" variant="primary">Start</Button>
+      </Form>
+      
+      <Form onSubmit={onGuess} className="my-5">
         <Form.Group>
           <Form.Label>{`Guess the number between ${min} to ${max}`}</Form.Label>
-          <Form.Control type="text" name="user" placeholder="Enter Name" />
+          <Form.Control type="text" name="guess" placeholder="Enter Name" />
           <Form.Text>{display}</Form.Text>
         </Form.Group>
       </Form>
