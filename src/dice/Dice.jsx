1cf4572245158dcf189c51eae6ad852b989fad8a
Laviel <fafansin@gmail.com>
2023-12-12 12:18:41 +0300

    refactor state management for dice

--- src/dice/Dice.jsx
+++ src/dice/Dice.jsx
@@ -3,19 +3,13 @@ import { v4 as uuidv4 } from 'uuid';
 import Die from './Die';
 import Button from 'react-bootstrap/Button';
 
-// import {} from '
-
-
 function Dice({nDices=2}) {
-  const [ faces, setFaces ] = useState([]);
-  const [ rolling, setRolling ] = useState(false);
+  const [ state, setState] = useState({faces:[], rolling:false});
   const mounted = useRef();
   
-
   useEffect(() => {
     if(!mounted.current){
       mounted.current = true;
-      // rollDice()
       rollDice();
     }
   });
@@ -25,21 +19,23 @@ function Dice({nDices=2}) {
     for(let i = 0; i < nDices; i++){
       ref.push(getRandom());
     }
-    setRolling(true);
-    setFaces(ref);
-    setTimeout(() => {setRolling(false)}, 1000)
+    setState({...state, rolling:true});
+    
+    setTimeout(() => {
+      setState({faces:ref, rolling:false})
+    }, 1000, ref)
   }
   function getRandom(){
     return Math.floor(Math.random() * 6);
-    
   }
+
   return (
     <div className="Dice shadow border text-center p-5">
       <h1 className="h1 mb-3">Roll Dice</h1>
       <div className="dice-wrap d-flex mb-5">
-        { faces.map((face)=> <Die key={uuidv4()} face={face} rolling={rolling}/>)}
+        { state.faces.map((face)=> <Die key={uuidv4()} face={face} rolling={state.rolling}/>)}
       </div>
-      <Button variant="primary" onClick={rollDice} disabled={rolling}>{rolling ? 'rolling..' : "Roll Dice!"}</Button>
+      <Button variant="primary" onClick={rollDice} disabled={state.rolling}>{state.rolling ? 'rolling..' : "Roll Dice!"}</Button>
     </div>
   )
 }
