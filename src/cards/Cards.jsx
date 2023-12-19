4ab1a293923f243376150b063dc9b10f961e9143
Laviel <fafansin@gmail.com>
2023-12-19 12:41:20 +0300

    Completed card dealer component

--- src/cards/Cards.jsx
+++ src/cards/Cards.jsx
@@ -1,14 +1,10 @@
 import { useEffect, useRef, useState } from 'react';
 import './Cards.scss';
-import Card from './Card';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 
 function Cards() {
-  const [ cards, setCards ] = useState([]);
-  const [ deckId, setDeckId ] = useState(null);
-  const [ remaining, setRemaining ] = useState(52) ;
-
+  const [ state, setState ] = useState({cards:[], deckId:null, remaining:0, loading:false});
   const mounted = useRef();
   
   useEffect(()=>{
@@ -17,43 +13,52 @@ function Cards() {
       // initialize deck
       shuffle();
     }
-    return () => {
-      setDeckId(null);
-      setRemaining(52);
-    }
   });
 
   async function shuffle(){
+    setState({...state, loading:true, remaining:0});
     try{
       const  { data } = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-      console.log(data)
-      setDeckId(data.deck_id);
-      setRemaining(data.remaining);
+      setState({...state, deckId:data.deck_id, remaining:data.remaining, loading:false, cards:[]});
     }catch(error){
       console.error("Error Shuffle", error);
     }
-    
   }
 
   async function getCard(){
+    setState({...state, loading:true})
     try {
-      const { data } = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);  
-      console.log("GET CARD", data);
-      setCards([...cards, data.cards[0]]);
-      setRemaining(data.remaining);
+      const { data } = await axios.get(`https://deckofcardsapi.com/api/deck/${state.deckId}/draw/?count=1`);  
+      setState({...state, 
+          loading:false,
+          cards:[...state.cards, {
+            code:data.cards[0].code, 
+            url:data.cards[0].images.png, 
+            rotation:Math.floor(Math.random() * 18) * (Math.random() < 0.5 ? -1 : 1)}], 
+          remaining:data.remaining});
     } catch (error) {
       console.error("ERROR get Card", error);
     }
     
   }
   return (
-    <div className="Cards">
-      <h1>PANIS {deckId} NATITIRA {remaining}</h1>
-      <Button variant="primary" onClick={getCard}>GIMME A CARD!</Button>
-      <Button variant="info" onClick={shuffle}>SHUFFLE!</Button>
-      <div className="pile">
-        {cards.map((card) => (<img data={card} alt={card.code}/>))}
+    <div className="Cards text-center border shadow py-3">
+      <h1 className="text-center">Card Dealer</h1>
+      <div className="d-flex gap-3 justify-content-center">
+        <Button variant="primary" onClick={getCard} disabled={state.loading}>GIMME A CARD!</Button>
       </div>
+      <div className="pile mt-5">
+        {state.cards.map((card) => (
+          <img 
+            key={card.code} 
+            data={card} 
+            alt={card.code} 
+            src={card.url}
+            style={{rotate:`${card.rotation}deg`}}
+            />))}
+      </div>
+      <p>Cards Remaming: {state.remaining}</p>
+      <Button variant="info" onClick={shuffle}>SHUFFLE!</Button>
     </div>
   )
 }
