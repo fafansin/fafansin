9012b7b8abe620c2fe51551735b82a03bd0f2c09
Laviel <fafansin@gmail.com>
2023-12-20 13:26:52 +0300

    Completed dad jokes exercise

---
+++ src/jokes/Joke.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import {faFaceGrinSquintTears, faFaceLaugh, faFaceSmile, faFaceMeh, faFaceFrown} from '@fortawesome/free-solid-svg-icons';
+
+
+
+function Joke({id, joke, score=3, handleVote}) {
+
+  function handleUp(event){
+    handleVote({id:id, score:score+1})
+  }
+
+  function handleDown(event){
+    handleVote({id:id, score:score-1})
+  }
+  
+  return (
+    <div className="Joke border-bottom d-flex align-items-center">
+      <div className="votes d-flex">
+        <Button id="up" variant="outline" onClick={handleUp}><FontAwesomeIcon icon={faArrowUp} /></Button>
+        <div className="score">{score}</div>
+        <Button id="down" variant="outline" onClick={handleDown}><FontAwesomeIcon icon={faArrowDown} /></Button>
+      </div>
+      <div className="d-flex justify-content-between align-items-center gap-2" style={{width:"100%"}}>
+        <p>{joke}</p>
+        <div className="rate">
+          <FontAwesomeIcon icon={score > 14 ? faFaceGrinSquintTears : score > 9 ? faFaceLaugh :  score > 4 ? faFaceSmile : score > 2 ? faFaceMeh :faFaceFrown} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Joke
--- src/jokes/JokeList.jsx
+++ src/jokes/JokeList.jsx
@@ -1,36 +1,61 @@
 import { useState, useEffect, useRef} from 'react'
 import axios from 'axios';
 import './JokeList.scss';
+import Joke from './Joke';
+import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFaceLaugh, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 function JokeList({nJokes=10}) {
-  const [ state, setState ] = useState({jokes:[]})
+  const [ state, setState ] = useState({loading:false, jokes:JSON.parse(window.localStorage.getItem('jokes') || "[]")})
   const mounted = useRef();
   useEffect(()=>{
     if(!mounted.current){
       mounted.current = true;
-
-      getJokes()
+      if(!state.jokes.length){
+        getJokes();
+      }
     }
-
+    window.localStorage.setItem(
+      "jokes",
+      JSON.stringify(state.jokes)
+    )
   })
+  function moreJokes(event){
+    getJokes();
+  }
 
   async function getJokes(){
+    setState({...state, loading:true});
     const jokes = [];
     while(jokes.length < nJokes){
       const res = await axios.get('https://icanhazdadjoke.com/', {headers:{Accept:'application/json'}});
-      jokes.push(res.data);
-      console.log('Getting', jokes)
+      if(!jokes.find((element) => element.id === res.data.id)){
+        jokes.push({id:res.data.id, joke:res.data.joke, score:0});
+      }
     }
-    
-    
-    setState({...state, jokes:jokes});
-
+    setState({...state, jokes:[...state.jokes, ...jokes], loading:false});
   }
+
+  function vote(data){
+    const newJokes = state.jokes.map((item) => (item.id === data.id ? {...item, score:data.score} : item)).sort((a,b) => b.score-a.score);
+    setState({...state, jokes:newJokes});
+  }
+
   return (
     <div className="JokeList">
-      <h1 className="h1">Dad Jokes</h1>
-      <ul>
-        {state.jokes.map(joke => <li key={joke.id}>{joke.joke}</li>)}
+      <div className="left-panel">
+        <h1 className="h1">Dad Jokes</h1>
+        <div className="face">
+          <FontAwesomeIcon icon={faFaceLaugh} />
+        </div>
+        <Button onClick={moreJokes} variant="info">More Jokes</Button>
+
+      </div>
+      <ul className="right-panel shadow">
+        {state.loading ? (<div className="spinner"><FontAwesomeIcon icon={faSpinner} spin size="2xl" /></div>) : 
+        state.jokes.map(joke => <li key={joke.id}><Joke id={joke.id} joke={joke.joke} score={joke.score} handleVote={vote}/></li>)
+        }
       </ul>
     </div>
   )
