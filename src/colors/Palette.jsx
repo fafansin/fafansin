5d406e2478bb4f32e00e7efc1d3f1600deeb7720
Laviel <fafansin@gmail.com>
2023-12-27 11:15:28 +0300

    add chroma js and color heloper to generate multitude of colors

--- src/colors/Palette.jsx
+++ src/colors/Palette.jsx
@@ -3,6 +3,7 @@ import ColorBox from './ColorBox';
 import { v4 as uuidv4 } from 'uuid';
 import './Palette.scss';
 
+
 function Palette({data}) {
   return (
     <div className="Palette">
--- src/colors/Palettes.jsx
+++ src/colors/Palettes.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import seedColors from './seedColors.js';
 import Palette from './Palette';
+import { generatePalette } from './colorHelpers';
 
 function Palettes() {
+  console.log(generatePalette(seedColors[4]));
   return (
     <div className="Palettes">
       <Palette data={seedColors[0]} />
