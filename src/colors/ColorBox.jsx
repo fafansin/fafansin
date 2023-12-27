3103739fd02a79dfa79018b7d01eb6cbb7f1bbda
Laviel <fafansin@gmail.com>
2023-12-27 10:47:54 +0300

    animated color boxes when copied

--- src/colors/ColorBox.jsx
+++ src/colors/ColorBox.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import { useState } from 'react';
 import './ColorBox.scss';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 function ColorBox({color, name}) {
+  const [ copied, setCopied ] = useState(false);
+  
+  function onCopy(event){
+    setCopied(true);
+  }
+
   return (
-    <CopyToClipboard text={color}>
+    <CopyToClipboard text={color} onCopy={onCopy}>
       <div className="ColorBox" style={{backgroundColor:color}}>
+        <div className={`copy-overlay ${copied && 'show'}`} style={{backgroundColor:color}}></div>
+        <div className={`copy-msg ${copied && 'show'}`}>
+          <h1>copied!</h1>
+          <p>{color}</p>
+        </div>
         <div className="copy-container">
           <div className="box-content">
             <span>{name}</span>
