adc57fae199a4bdcedaae986c123d65b3dea624f
Laviel <fafansin@gmail.com>
2023-12-28 09:57:37 +0300

    aligned format drop down

--- src/colors/PaletteHeader.jsx
+++ src/colors/PaletteHeader.jsx
@@ -12,16 +12,18 @@ function PaletteHeader({level, changeLevel, format, changeFormat}) {
         <AdUnitsIcon/>
         <a href="/">reactcolorpicker</a>
       </div>
-      <div className="slider-container">
-        <span>Level: {level}</span>
-        <Form.Range className="slider" value={level} min="100" max="900" step="100" onChange={changeLevel}/>
-      </div>
-      <div className="select-container">
-        <Select name="format" onChange={changeFormat} label="Format" value={format}>
-          <MenuItem value="hex">HEX - #ffffff</MenuItem>
-          <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
-          <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
-        </Select>
+      <div className="wrap">
+        <div className="slider-container">
+          <span>Level: {level}</span>
+          <Form.Range className="slider" value={level} min="100" max="900" step="100" onChange={changeLevel}/>
+        </div>
+        <div className="select-container">
+          <Select name="format" onChange={changeFormat} label="Format" value={format}>
+            <MenuItem value="hex">HEX - #ffffff</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
+          </Select>
+        </div>
       </div>
       
     </header>
