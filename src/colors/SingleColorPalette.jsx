aa050d3b8acbcc5d83edcd25174352486ae4d301
Laviel <fafansin@gmail.com>
2023-12-28 14:17:15 +0300

    retrieving data from singlecolorpalette

--- src/colors/SingleColorPalette.jsx
+++ src/colors/SingleColorPalette.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { generatePalette } from './colorHelpers';
 import seedColors from './seedColors.js'
 
 function SingleColorPalette() {
-  console.log(seedColors)
   const { paletteId, colorId } = useParams();
+
+  function findPalette(id){
+    return seedColors.find((palette) => palette.id === id)
+  }
+  
+  const palette = generatePalette(findPalette(paletteId))
+  console.log("YARI KA", palette);
+
 
   return (
     <div className="SingleColorPalette">
